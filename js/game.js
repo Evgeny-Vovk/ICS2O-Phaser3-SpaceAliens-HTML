f9e47bb7a9edf5772ba6086858072c7020da2e49
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-05-06 18:13:17 +0000

    Finish phaser 1

--- js/game.js
+++ js/game.js
@@ -1,49 +1,11 @@
 /* global Phaser */
 
-// Created by: Evgeny Vovk
-// Created on: May 2022
-// This is the Phaser3 game configuration file
-
-// scene import statements
-import SplashScene from "./splashScene.js"
-import TitleScene from "./titleScene.js"
-import MenuScene from "./menuScene.js"
-
-// create the new scenes
-const splashScene = new SplashScene()
-const titleScene = new TitleScene()
-const menuScene = new MenuScene()
-
-/**
- * Start Phaser Game.
- */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: true,
-    },
-  },
-  // set background color
-  backgroundColor: 0x5f6e7a,
-  scale: {
-    mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page.
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
+  backgroundColor: 0x5f6e7a
 }
 
-const game = new Phaser.Game(config)
-// console.log(game)
-
-// load scenes
-// Note: remember any "key" is global and CAN NOT be reused!
-game.scene.add("splashScene", splashScene)
-game.scene.add("titleScene", titleScene)
-game.scene.add("menuScene", menuScene)
-
-// the start scene
-game.scene.start("splashScene")
+const game = new Phaser.GAME(config)
+console.log(game)
