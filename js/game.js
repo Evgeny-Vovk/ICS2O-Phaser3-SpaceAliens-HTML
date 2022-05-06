dc29e5668547283bda3de4e28a8e77ca105f25cd
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-05-06 21:02:35 +0000

    Finish phaser3-02

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Evgeny Vovk All rights reserved
+//
+// Created by: Evgeny Vovk
+// Created on: May 2022
+// This is the Phaser3 game configuration file
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
